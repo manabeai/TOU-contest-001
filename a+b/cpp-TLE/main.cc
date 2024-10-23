da739d90f74a4d790c0602a9e58294e759194420
manabeai <matsu.devtool@gmail.com>
2024-10-23 15:18:05 +0900

    TLE解放を適切に設定

--- a+b/cpp-TLE/main.cc
+++ a+b/cpp-TLE/main.cc
@@ -6,12 +6,12 @@ int main() {
   int a, b;
   cin >> a >> b;
   int c = 0;
-  for (int i = 0; i < a; ++i) {
+  for (int i = 0; i < 10000000000; ++i) {
     c++;
   }
   for (int i = 0; i < b; ++i) {
     c++;
   }
-  cout << c << endl;
+  cout << a+b << endl;
   return 0;
 }
