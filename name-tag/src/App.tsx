a966845f3d9ec2c87c3c0b85119acc0a290c3829
manabeai <matsu.devtool@gmail.com>
2025-01-14 23:29:46 +0900

    initial

---
+++ name-tag/src/App.tsx
@@ -0,0 +1,15 @@
+
+// import './App.css'
+// import 'virtual:uno.css';
+function App() {
+
+  return (
+    <>
+      <main>
+        hello world!!
+      </main>
+    </>
+  )
+}
+
+export default App
