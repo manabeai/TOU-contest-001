fc4ed6d878f0ac37b3ea06477bb5acb7ba227b41
manabeai <matsu.devtool@gmail.com>
2025-01-14 23:40:00 +0900

    ネームタグ

--- name-tag/src/App.tsx
+++ name-tag/src/App.tsx
@@ -5,9 +5,9 @@ function App() {
 
   return (
     <>
-      <main>
-        hello world!!
-      </main>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
