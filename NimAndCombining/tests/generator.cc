957a333c47be0d0517d5825cfab9c63b022b7f61
uruzunyaa <uruzunyaa@gmail.com>
2025-02-01 19:40:52 +0900

    問題文を作成、

---
+++ NimAndCombining/tests/generator.cc
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define rep(i,n) for (long long i=0;i<(ll)n;i++)
+
+
+
+//乱数
+random_device rnd;// 非決定的な乱数生成器
+mt19937 mt(rnd());// メルセンヌ・ツイスタの32ビット版、引数は初期シード
+#define pmt(mn,mx) (mt()%(mx-mn+1)+mn)
+
+void make(ll num,string name,ll MIN_N,ll MAX_N,ll MIN_A,ll MAX_A){
+	rep(i,num){
+		ofstream fout("./"+name+".in");//出力する場合の出力先を指定
+		
+	}
+	
+}
+
+
+//メイン
+int main(){
+
+	
+	return 0;
+}
