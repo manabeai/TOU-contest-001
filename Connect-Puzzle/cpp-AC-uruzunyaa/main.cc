cf4ff955487d6a93d8fc04523f402b62cda6d0b1
uruzunyaa <uruzunyaa@gmail.com>
2025-02-06 08:11:47 +0900

    make-Connect-Puzzle

---
+++ Connect-Puzzle/cpp-AC-uruzunyaa/main.cc
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define rep(i,n) for (long long i=0;i<(ll)n;i++)
+#define loop(i,m,n) for(long long i=m;i<=(ll)n;i++)
+#define vl vector<long long>
+#define vvl vector<vector<long long>>
+#define vdbg(a) rep(ii,a.size()){cout<<a[ii]<<" ";}cout<<endl;
+#define vvdbg(a) rep(ii,a.size()){rep(jj,a[ii].size()){cout<<a[ii][jj]<<" ";}cout<<endl;}
+#define setdbg(a) for(const auto & ii:a){cout<<ii<<" ";}cout<<endl;
+//メイン
+int main(){
+	ll n;
+	cin>>n;
+	vvl ans(n,vl(n));
+	if(n<=3){
+		if(n==1)cout<<"No"<<endl;
+		if(n==2){
+			cout<<"Yes"<<endl;
+			cout<<"1 2"<<endl;
+			cout<<"3 4"<<endl;
+		}
+		if(n==3){
+			cout<<"Yes"<<endl;
+			cout<<"2 2 3"<<endl;
+			cout<<"2 2 3"<<endl;
+			cout<<"3 3 1"<<endl;
+		}
+		return 0;
+	}
+
+	vl a={3,3,2,2};
+	
+	vl tmp;
+	rep(i,n-1)tmp.push_back(a[i%4]);
+
+	if(n%4==2){
+		tmp.pop_back();
+		tmp.push_back(1);
+		tmp.push_back(3);
+	}else{
+		tmp.push_back(1);
+	}
+	
+	if(n==7)tmp={3,3,3,2,3,2,1};
+
+	rep(i,n)rep(j,n)ans[i][(j+i*2)%n]=tmp[j];
+	
+	cout<<"Yes"<<endl;
+	vvdbg(ans);
+	return 0;
+}
---
+++ Connect-Puzzle/tests/generator.cc
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define rep(i,n) for (long long i=0;i<(ll)n;i++)
+#define vout(a) for (long long i=0;i<(ll)a.size()-1;i++){fout<<a[i]<<" ";}fout<<a[a.size()-1]<<endl
+
+
+//乱数
+random_device rnd;// 非決定的な乱数生成器
+mt19937_64 mt(rnd());// メルセンヌ・ツイスタの32ビット版、引数は初期シード
+#define pmt(mn,mx) (mt()%(mx-mn+1)+mn)
+
+void make(ll num,string name,ll MIN_N,ll MAX_N){
+	rep(t,num){
+		ofstream fout("./"+name+to_string(t+1)+".in");//出力する場合の出力先を指定
+		ll n;
+		n=pmt(MIN_N,MAX_N);
+		fout<<n<<endl;
+	}
+}
+
+
+//メイン
+int main(){
+	make(1,"sample_",4,4);
+	make(30,"random_",14,996);
+	make(1,"hand1_",1,1);
+	make(1,"hand2_",2,2);
+	make(1,"hand3_",3,3);
+	make(1,"hand4_",5,5);
+	make(1,"hand5_",6,6);
+	make(1,"hand6_",7,7);
+	make(1,"hand7_",8,8);
+	make(1,"hand8_",9,9);
+	make(1,"hand9_",10,10);
+	make(1,"hand10_",11,11);
+	make(1,"hand11_",12,12);
+	make(1,"hand12_",13,13);
+	make(1,"hand13_",997,997);
+	make(1,"hand14_",998,998);
+	make(1,"hand15_",999,999);
+	make(1,"hand16_",1000,1000);
+	return 0;
+}
