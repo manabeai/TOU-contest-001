3e12a461c3e332f2363907870725632ea6630a63
manabeai <matsu.devtool@gmail.com>
2025-02-09 17:15:18 +0900

    generatorを追加

--- meteo/tests/generator.py
+++ meteo/tests/generator.py
@@ -2,24 +2,23 @@
 
 import random
 
-import six
-
-MAX = 1000000000
 seq = 0
 
-
-def Generate(a, b):
+def Generate():
     global seq
+    H = random.randint(1, 10)
+    W = random.randint(1, 10)
     filename = '50-random%02d.in' % seq
     with open(filename, 'w') as f:
-        f.write('{} {}\n'.format(a, b))
+        f.write(f"{H} {W}\n")
+        for i in range(H):
+            row = [str(random.randint(0, 100)) for _ in range(W)]
+            f.write(" ".join(row) + "\n")
     seq += 1
 
-
 def main():
-    for _ in six.moves.range(20):
-        Generate(random.randrange(0, MAX), random.randrange(0, MAX))
+    for _ in range(20):
+        Generate()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
