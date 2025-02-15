a327f33cf087155f9dbc43b4339629618c585d8c
orca <fiveofwell777@gmail.com>
2025-02-15 20:44:59 +0900

    testlibを用いてgenerator,validator再作成、問題idをCに決定、handケース作成

--- station-count/tests/validator.cc
+++ station-count/tests/validator.cc
@@ -1,57 +1,38 @@
+#include "testlib.h"
 #include <iostream>
 #include <string>
 #include <unordered_set>
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+    registerValidation(argc, argv);
     try {
-        string T, U;
-        int N;
-        cin >> T >> U >> N;
-        if (T.length() < 1 || T.length() > 20) {
-            throw runtime_error("T Invalid Length");
+        string A = inf.readToken("[a-zA-Z]{1,20}");
+        inf.readEoln();
+        string B = inf.readToken("[a-zA-Z]{1,20}");
+        inf.readEoln();
+        int N = inf.readInt(2,1000);
+        inf.readEoln();
+
+        if(A == B){
+            throw runtime_error("A And B Are The Same");
         }
-        for (const char& c : T) {
-            if ((c < 'A' || c > 'Z') && (c < 'a' || c > 'z')) {
-                throw runtime_error("T Invalid Character");
-            }
-        }
-        if (U.length() < 1 || U.length() > 20) {
-            throw runtime_error("U Invalid Length");
-        }
-        for (const char& c : U) {
-            if ((c < 'A' || c > 'Z') && (c < 'a' || c > 'z')) {
-                throw runtime_error("U Invalid Character");
-            }
-        }
-        if(T == U){
-            throw runtime_error("T And U Are The Same");
-        }
-        if (N < 2 || N > 1000) {
-            throw runtime_error("N Out Of Range");
-        }
+
         unordered_set<string> s_set;
         for (int i = 0; i < N; i++) {
-            string S;
-            cin >> S;
-            if (S.length() < 1 || S.length() > 20) {
-                throw runtime_error("S Invalid Length");
-            }
+            string S = inf.readToken("[a-zA-Z]{1,20}");
+            if(i != N - 1)inf.readEoln();
             if (!s_set.insert(S).second) {
                 throw runtime_error("Duplicated S");
             }
-            for (const char& c : S) {
-                if ((c < 'A' || c > 'Z') && (c < 'a' || c > 'z')) {
-                    throw runtime_error("S Invalid Character");
-                }
-            }
         }
-        if (s_set.insert(T).second) {
-            throw runtime_error("T Is Not Included In S");
+        if (s_set.insert(A).second) {
+            throw runtime_error("A Is Not Included In S");
         }
-        if (s_set.insert(U).second) {
-            throw runtime_error("U Is Not Included In S");
+        if (s_set.insert(B).second) {
+            throw runtime_error("B Is Not Included In S");
         }
+        inf.readEof();
         return 0;
     }
     catch (const runtime_error& e) {
