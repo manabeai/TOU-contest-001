7c2a9232217c9b7e2ee9f35ac9160b48888a159a
manabeai <matsu.devtool@gmail.com>
2025-02-16 12:27:16 +0900

    validatorを追加

--- meteo/tests/validator.py
+++ meteo/tests/validator.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python
 
-import random
+import re
+import sys
 
-def generate_test_case():
-    H = random.randint(1, 10)
-    W = random.randint(1, 10)
-    
-    print(f"{H} {W}")
-    
+H_MIN = 1
+H_MAX = 10
+
+W_MIN = 1
+W_MAX = 10
+
+A_MIN = 0
+A_MAX = 100
+
+
+def main():
+    m = re.match(r'^(\d+) (\d+)\n$', sys.stdin.readline())
+    assert m, 'Does not match with regexp'
+    H, W = map(int, m.groups())
+    assert H_MIN <= H <= H_MAX, 'a out of range: %d' % H
+    assert W_MIN <= W <= W_MAX, 'a out of range: %d' % W
+
     for i in range(H):
-        row = [str(random.randint(0, 100)) for _ in range(W)]
-        print(" ".join(row))
+        m = re.match(r'^\d+(?:\s+\d+)*$', sys.stdin.readline())
+        A = list(map(int, m.group().split()))
+        assert len(A) == W, 'invalid length'
+        for a in A:
+            assert A_MIN <= a <= A_MAX, 'a out of range: %d' % a
+    
+    assert sys.stdin.read() == '', 'extra data'
 
-if __name__ == "__main__":
-    generate_test_case()
+if __name__ == '__main__':
+    main()
